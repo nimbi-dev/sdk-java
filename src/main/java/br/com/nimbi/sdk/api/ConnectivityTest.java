725bc577c9d7ad37105f234f12138859b2f6870d
jpmo22 <jpmo22@gmail.com>
2018-05-21 15:29:22 -0300

    Fixing misspelling and more
    Renaming the class to fixing misspelling and adding documentation.

--- src/main/java/br/com/nimbi/sdk/api/ConnectivityTest.java
+++ src/main/java/br/com/nimbi/sdk/api/ConnectivityTest.java
@@ -8,12 +8,29 @@ import okhttp3.Response;
 import br.com.nimbi.sdk.Callable;
 import br.com.nimbi.sdk.NimbiClient;
 
-public class ConectivityTest extends Callable {
+/**
+ * Java wrapper for Nimbi API <b>ConectivityTest</b>.
+ * 
+ * @see <a href="https://nim.bi/api">https://nim.bi/api</a>
+ * @author jpmo22
+ * 
+ */
+public class ConnectivityTest extends Callable {
 
-    public ConectivityTest( final NimbiClient client ) {
+    /**
+     * @param client
+     *            Defines environment and access key for Nimbi APIs.
+     * 
+     */
+    public ConnectivityTest( final NimbiClient client ) {
         super( client );
     }
 
+    /**
+     * @return A JSON representation of the current client basic info.
+     * @throws IOException
+     *             if any unexpected error occurs.
+     */
     public Response testConectivity() throws IOException {
         final OkHttpClient client = new OkHttpClient();
 
