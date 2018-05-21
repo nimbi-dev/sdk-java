c609a2fb0624e415fa0c1c08a8652e189de1d169
jpmo22 <jpmo22@gmail.com>
2018-05-21 15:27:00 -0300

    Minor fixes and Documentations
    Some issues fixes and Javadocs.

--- src/main/java/br/com/nimbi/sdk/Callable.java
+++ src/main/java/br/com/nimbi/sdk/Callable.java
@@ -2,27 +2,39 @@ package br.com.nimbi.sdk;
 
 import okhttp3.Request;
 
+/**
+ * A class to enhance all API calls with API keys.
+ * 
+ * @author jpmo22
+ */
 public class Callable {
 
-    private Request.Builder builder;
+    private final Request.Builder builder;
 
-    private NimbiClient client;
+    private final NimbiClient client;
 
-    private Callable() {
-
-    }
-
+    /**
+     * @param client
+     *            The Nimbi client.
+     */
     public Callable( final NimbiClient client ) {
+        super();
         this.client = client;
         this.builder =
-                new Request.Builder().addHeader( NimbiClient.ID, client.getApiId() ).addHeader(
-                        NimbiClient.KEY, client.getApiKey() );
+                new Request.Builder().addHeader( NimbiClient.API_ID, this.client.getApiId() )
+                        .addHeader( NimbiClient.API_KEY, this.client.getApiKey() );
     }
 
+    /**
+     * @return Enhanced request builder with Nimbi client
+     */
     public Request.Builder getBuilder() {
         return this.builder;
     }
 
+    /**
+     * @return The environment.
+     */
     public String getHost() {
         return this.client.getEnvironment().getHost();
     }
--- src/main/java/br/com/nimbi/sdk/NimbiClient.java
+++ src/main/java/br/com/nimbi/sdk/NimbiClient.java
@@ -1,14 +1,38 @@
 package br.com.nimbi.sdk;
 
+/**
+ * A client to connect with Nimbi API.
+ * 
+ * @author jpmo22
+ * 
+ */
 public class NimbiClient {
 
-    public static final String ID = "ClientAPI_ID";
+    /**
+     * HTTP Header name for API ID.
+     */
+    public static final String API_ID = "ClientAPI_ID";
 
-    public static final String KEY = "ClientAPI_Key";
+    /**
+     * HTTP Header name for API Key.
+     */
+    public static final String API_KEY = "ClientAPI_Key";
 
+    /**
+     * Available environments for Nimbi APIs.
+     * 
+     * @author jpmo22
+     */
     public enum Environment {
 
+        /**
+         * Quality and Assurance environment.
+         */
         QA( "https://api01-qa.nimbi.net.br" ),
+
+        /**
+         * Production environment.
+         */
         PRODUCTION( "https://api001.nimbi.com.br" );
 
         private Environment( final String host ) {
@@ -17,36 +41,52 @@ public class NimbiClient {
 
         private String host;
 
+        /**
+         * @return Environment host.
+         */
         public String getHost() {
             return this.host;
         }
     }
 
-    private String apiId;
+    private final String apiId;
 
-    private String apiKey;
+    private final String apiKey;
 
-    private Environment environment;
+    private final Environment environment;
 
-    private NimbiClient() {
-        super();
-    }
-
-    private NimbiClient( final Environment environment, final String apiId, final String apiKey ) {
+    /**
+     * @param environment
+     *            The target environment.
+     * @param apiId
+     *            The API ID.
+     * @param apiKey
+     *            The API Key.
+     */
+    public NimbiClient( final Environment environment, final String apiId, final String apiKey ) {
         super();
         this.environment = environment;
         this.apiId = apiId;
         this.apiKey = apiKey;
     }
 
+    /**
+     * @return The API ID.
+     */
     public String getApiId() {
         return this.apiId;
     }
 
+    /**
+     * @return The API Key.
+     */
     public String getApiKey() {
         return this.apiKey;
     }
 
+    /**
+     * @return The environment.
+     */
     public Environment getEnvironment() {
         return this.environment;
     }
