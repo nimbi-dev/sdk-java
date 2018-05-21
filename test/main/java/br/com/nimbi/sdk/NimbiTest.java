3ee042b0a76a6ed2e67c793ab18e049db3b8487d
jpmo22 <jpmo22@gmail.com>
2018-05-21 15:30:05 -0300

    Adding JUnit
    If is not tested is not done.

---
+++ test/main/java/br/com/nimbi/sdk/NimbiTest.java
@@ -0,0 +1,39 @@
+package br.com.nimbi.sdk;
+
+import java.io.IOException;
+
+import okhttp3.Response;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.nimbi.sdk.NimbiClient.Environment;
+import br.com.nimbi.sdk.api.ConnectivityTest;
+
+/**
+ * A simple JUnit Test for Java Wrapper Nimbi API.
+ * 
+ * @author jpmo22
+ */
+public class NimbiTest extends Assert {
+
+    /**
+     * Testing an invalid ClientAPI_ID and ClientAPI_Key call.
+     * 
+     * @throws IOException
+     *             if any unexpected error occurs.
+     */
+    @Test
+    public void testConectivity() throws IOException {
+
+        final NimbiClient client =
+                new NimbiClient( Environment.QA, "[some_client_api_id]", "[some_client_api_key]" );
+
+        final ConnectivityTest connectivityTest = new ConnectivityTest( client );
+        final Response response = connectivityTest.testConectivity();
+        assertEquals(
+                "{\"Errors\":[\"Invalid API Key. ([some_client_api_key])\"],\"StatusCode\":400}",
+                response.body().string() );
+    }
+
+}
